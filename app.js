b821f51c16e492eeca2e4397d8dbc2aab41132e5
Morsi <136155900+Mahmoud-Morsi0@users.noreply.github.com>
2024-04-23 14:15:15 +0200

    edit port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Connectione a MongoDB
 mongoose
